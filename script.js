293fce9ed91eebc2a5ccbf2c51159f7aa4c57a68
littleyk <81969367+littleyk@users.noreply.github.com>
2024-12-30 21:58:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ var confetti = new ConfettiGenerator(confettiSettings);
 confetti.render();
 
  let p = document.createElement("p");
- p.innerText = "Congrats you made the right choice 🎉😉";
+ p.innerText = "Congrats you made the right choice 🎉";
   p.style.fontSize = "2rem";
   p.style.fontWeight = "bold";
   p.style.textAlign = "center";
